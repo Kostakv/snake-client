d6d83a53c55f96544611d9659bb234e022971f90
Konstantin <kostakv@outlook.com>
2022-04-26 01:47:05 +0000

    [update]: updated play.js keyboard events

--- play.js
+++ play.js
@@ -4,6 +4,22 @@ const { connect } = require("./client");
 
 // establishes a connection with the game server
 
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
 console.log("Connecting ...");
 
