777487b8a61c87e9c8852a8658b8d95d543316d2
Konstantin <kostakv@outlook.com>
2022-04-26 00:45:08 +0000

    [update]: updated client and play js files

--- client.js
+++ client.js
@@ -1,1 +1,24 @@
-const myObject = require("./play");
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+
+    
+  });
+  conn.on("connect", () => {
+    console.log("you died due to afk :(")
+    return conn
+  });
+
+
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+
+module.exports = { connect };
+
--- play.js
+++ play.js
@@ -1,23 +1,9 @@
 const net = require("net");
+const { connect } = require("./client");
+
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
 
-    
-  });
-  conn.on("connect", () => {
-    console.log("you died due to afk :(")
-    return conn
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
